2e4c80bd31a14a7471314b25e3bafe5da81ebc1c
HimajaHimaja <143433293+HimajaHimaja@users.noreply.github.com>
2024-07-04 13:59:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-// function toggleMode() {
-//     const root = document.documentElement;
-   
 
-//     const toggleButton = document.querySelector('.toggle');
-//     if (root.style.getPropertyValue('--background-color') === 'var(--background-color-dark)') {
-//         root.style.setProperty('--background-color', 'var(--background-color-light)');
-//         root.style.setProperty('--main-background-color', 'var(--main-background-color-light)');
-//         root.style.setProperty('--highlighting-color', 'var(--highlighting-color-light)');
-//         root.style.setProperty('--primary-heading-color', 'var(--primary-heading-color-light)');
-//         root.style.setProperty('--text-color', 'var(--text-color-light)');
-//         root.style.setProperty('--secondary-heading-color', 'var(--secondary-heading-color-light)');
-//         root.style.setProperty('--nav-background-color', 'var(--nav-background-color-light)');
-//         root.style.setProperty('--nav-text-color', 'var(--nav-text-color-light)');
-//         toggleButton.textContent = '☀️';
-//     } else {
-//         root.style.setProperty('--background-color', 'var(--background-color-dark)');
-//         root.style.setProperty('--main-background-color', 'var(--main-background-color-dark)');
-//         root.style.setProperty('--highlighting-color', 'var(--highlighting-color-dark)');
-//         root.style.setProperty('--primary-heading-color', 'var(--primary-heading-color-dark)');
-//         root.style.setProperty('--text-color', 'var(--text-color-dark)');
-//         root.style.setProperty('--secondary-heading-color', 'var(--secondary-heading-color-dark)');
-//         root.style.setProperty('--nav-background-color', 'var(--nav-background-color-dark)');
-//         root.style.setProperty('--nav-text-color', 'var(--nav-text-color-dark)');
-//         toggleButton.textContent = '🌙';
-//     }
-
-
-// }
 
 function toggleMode() {
     const root = document.documentElement;
@@ -40,11 +12,11 @@ function toggleMode() {
     if (isDarkMode) {
         console.log('Switching to light mode');
         root.style.setProperty('--background-color', 'var(--background-color-light)');
-        profileImage.setAttribute('src', 'images/white-back-img.jpg');
+        profileImage.setAttribute('src', 'white-back-img.jpg');
     } else {
         console.log('Switching to dark mode');
         root.style.setProperty('--background-color', 'var(--background-color-dark)');
-        profileImage.setAttribute('src', 'images/dark-back-img.jpg');
+        profileImage.setAttribute('src', 'dark-back-img.jpg');
     }
 
     console.log('Profile image source:', profileImage.getAttribute('src'));
